d47cd37eab0a24cd32cfbc4ec9ff57de086c6702
tmasushio <tmasushio@microsoft.com>
2023-01-08 16:47:20 +0900

    Added SDK code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
