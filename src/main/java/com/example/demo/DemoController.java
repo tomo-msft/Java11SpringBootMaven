95f57ffac9f95b5164c50bb907e01b338b2da60c
tmasushio <tmasushio@microsoft.com>
2024-02-03 12:07:01 +0900

    Fixed code

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -17,18 +17,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.applicationinsights.telemetry.Duration;
-import com.microsoft.applicationinsights.telemetry.RemoteDependencyTelemetry;
-import com.microsoft.applicationinsights.telemetry.SeverityLevel;
-
 @Controller
 public class DemoController {
-	static final TelemetryClient telemetryClient = new TelemetryClient();
     private static final Logger logger = LogManager.getLogger(DemoController.class);
 	private static final Logger logger2 = LogManager.getLogger("MyTest1");
 
-	private static final String AppName = "Java11SpringBootMaven-AiV3Sdk01";
+	private static final String AppName = "Java11SpringBootMaven-AiV3Sdk01-log4j2";
 
 	private String createLog(String message, String testName, Date now) {
 		return String.format(message + " TestName:" + testName + " AppName: " + AppName + " Date: " + now);
@@ -162,49 +156,4 @@ public class DemoController {
 		model.addAttribute("description", description);
 		return "testResult";
     }
-
-    @GetMapping("/sdk")
-    public String sdkTest (
-		@RequestParam(defaultValue = "SDK_Test") String name, Model model) {
-
-		Date now = new Date();
-		
-		// Event test
-		telemetryClient.trackEvent("Sample Event");
-		
-		// Metric test
-		telemetryClient.trackMetric("SampleMetric", 42.0);
-
-		// Dependency test
-		long startTime = System.currentTimeMillis();
-		long endTime = System.currentTimeMillis() + 100;
-		RemoteDependencyTelemetry telemetry = new RemoteDependencyTelemetry();
-		telemetry.setName("SampleName");
-		telemetry.setResultCode("499");
-		telemetry.setTarget("SampleTarget");
-		telemetry.setType("SampleType");
-		telemetry.setCommandName("SampleCommandName");
-		telemetry.setSuccess(false);
-		telemetry.setTimestamp(new Date(startTime));
-		telemetry.setDuration(new Duration(endTime - startTime));
-		telemetryClient.trackDependency(telemetry);
-
-		// Log test
-		Map<String, String> properties = new HashMap<>();
-		properties.put("Key1", "Value1");
-		properties.put("Key2", "Value2");
-		telemetryClient.trackTrace(createLog("Sample log", name, now), SeverityLevel.Warning, properties);
-
-		// Exception test
-		Exception ex = new Exception("Sample Exception");
-		telemetryClient.trackException(ex);
-
-		// set result
-		setResult(model, name, now);
-		Map<String, String> description = new HashMap<>();
-		description.put("status", "Success");
-		model.addAttribute("description", description);
-        return "testResult";
-    }
-
 }
